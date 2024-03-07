18d3e98fb86489028eab913192bbda3f960d2119
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-03-07 17:28:40 +0100

    create dumps and zip

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,11 +1,13 @@
 import re
 import requests
 import yaml
+import zipfile
 import sys
 from string import Template
 from SPARQLWrapper import SPARQLWrapper, JSON, TURTLE
 from urllib.parse import quote
 from os.path import join
+from os import remove
 from tqdm import tqdm
 
 try:
@@ -54,7 +56,7 @@ CONSTRUCT {
 }
 """)
 headers = {
-    "Accept": "application/x-trig"
+    "Accept": "text/plain"
 }
 
 queryTemplate = Template("""CONSTRUCT {
@@ -65,6 +67,13 @@ queryTemplate = Template("""CONSTRUCT {
 
 print("Exporting graphs")
 
+outputFileTTL = join(config['output'], 'dump.ttl')
+outputFileNQ = join(config['output'], 'dump.nq')
+
+# Empty output files
+open(outputFileTTL, 'w').close()
+open(outputFileNQ, 'w').close()
+
 for graph in tqdm(graphs):
     data = {
         "query": queryTemplate.substitute(graph=graph)
@@ -74,11 +83,19 @@ for graph in tqdm(graphs):
     except:
         print("Could not fetch data for", graph)
     if response:
-        output = response.content.decode()
-        # Insert name of named graph
-        output = re.sub(r'\n\{', '\n<' + graph + '> {', output, 1)
-        filename = quote(graph, safe='') + '.trig'
-        with open(join(config['output'], filename), 'w') as f:
-            f.write(output)
+        outputTTL = response.content.decode()
+        with open(outputFileTTL, 'a') as f:
+            f.write(outputTTL)
+        # For the NQ output, we insert the graph name at every line of outputTTL before the . at the end of the line
+        outputNQ = re.sub(r'\.$', '<%s> .' % graph, outputTTL, flags=re.MULTILINE)
+        with open(outputFileNQ, 'a') as f:
+            f.write(outputNQ)
+
+# Zip output files
+with zipfile.ZipFile(join(config['output'], 'dump.ttl.zip'), 'w') as zipf:
+    zipf.write(outputFileTTL, 'dump.ttl')
+
+with zipfile.ZipFile(join(config['output'], 'dump.nq.zip'), 'w') as zipf:
+    zipf.write(outputFileNQ, 'dump.nq')
 
 print("Successfully exported %d graphs" % len(graphs))
