b79606351aeaefde81ada2ce7617351a0574b04c
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-03-26 11:33:26 +0100

    output files

--- performBackup.sh
+++ performBackup.sh
@@ -27,7 +27,7 @@ rapper -f 'xmlns:crm="http://www.cidoc-crm.org/cidoc-crm"' \
   -f 'xmlns:prov="http://www.w3.org/ns/prov#"' \
   -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
   -f 'xmlns:frbroo="http://iflastandards.info/ns/fr/frbr/frbroo"' \
-  -i turtle -o turtle  ${OUTPUT_FOLDER}/dump.ttl.prefixed
+  -i turtle -o turtle ${OUTPUT_FOLDER}/dump.ttl > ${OUTPUT_FOLDER}/dump.ttl.prefixed
 
 mv ${OUTPUT_FOLDER}/dump.ttl.prefixed ${OUTPUT_FOLDER}/dump.ttl
 
@@ -42,7 +42,7 @@ rapper -f 'xmlns:crm="http://www.cidoc-crm.org/cidoc-crm"' \
   -f 'xmlns:prov="http://www.w3.org/ns/prov#"' \
   -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
   -f 'xmlns:frbroo="http://iflastandards.info/ns/fr/frbr/frbroo"' \
-  -i nquads -o nquads  ${OUTPUT_FOLDER}/dump.nq.prefixed
+  -i nquads -o nquads ${OUTPUT_FOLDER}/dump.nq > ${OUTPUT_FOLDER}/dump.nq.prefixed
 
 mv ${OUTPUT_FOLDER}/dump.nq.prefixed ${OUTPUT_FOLDER}/dump.nq
 
