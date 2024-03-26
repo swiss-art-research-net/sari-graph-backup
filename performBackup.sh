c56ba93a141e0e952a4d6eac792786aa49b26c3d
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-03-26 11:32:09 +0100

    prefix

--- performBackup.sh
+++ performBackup.sh
@@ -15,6 +15,37 @@ then
 fi
 
 docker exec ${PROJECT_NAME}_graph_backup python /scripts/backup.py ${CONFIG_FILE}
+
+rapper -f 'xmlns:crm="http://www.cidoc-crm.org/cidoc-crm"' \
+  -f 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' \
+  -f 'xmlns:rico="http://ica.org/standards/RiC/ontology#"' \
+  -f 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' \
+  -f 'xmlns:a="http://data.performing-arts.ch/a"' \
+  -f 'xmlns:x="http://data.performing-arts.ch/x"' \
+  -f 'xmlns:ldp="http://www.w3.org/ns/ldp#"' \
+  -f 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' \
+  -f 'xmlns:prov="http://www.w3.org/ns/prov#"' \
+  -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
+  -f 'xmlns:frbroo="http://iflastandards.info/ns/fr/frbr/frbroo"' \
+  -i turtle -o turtle  ${OUTPUT_FOLDER}/dump.ttl.prefixed
+
+mv ${OUTPUT_FOLDER}/dump.ttl.prefixed ${OUTPUT_FOLDER}/dump.ttl
+
+rapper -f 'xmlns:crm="http://www.cidoc-crm.org/cidoc-crm"' \
+  -f 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' \
+  -f 'xmlns:rico="http://ica.org/standards/RiC/ontology#"' \
+  -f 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' \
+  -f 'xmlns:a="http://data.performing-arts.ch/a"' \
+  -f 'xmlns:x="http://data.performing-arts.ch/x"' \
+  -f 'xmlns:ldp="http://www.w3.org/ns/ldp#"' \
+  -f 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' \
+  -f 'xmlns:prov="http://www.w3.org/ns/prov#"' \
+  -f 'xmlns:owl="http://www.w3.org/2002/07/owl#"' \
+  -f 'xmlns:frbroo="http://iflastandards.info/ns/fr/frbr/frbroo"' \
+  -i nquads -o nquads  ${OUTPUT_FOLDER}/dump.nq.prefixed
+
+mv ${OUTPUT_FOLDER}/dump.nq.prefixed ${OUTPUT_FOLDER}/dump.nq
+
 gzip -c ${OUTPUT_FOLDER}/dump.nq > ${OUTPUT_FOLDER}/dump.nq.gz
 gzip -c ${OUTPUT_FOLDER}/dump.ttl > ${OUTPUT_FOLDER}/dump.ttl.gz
 
